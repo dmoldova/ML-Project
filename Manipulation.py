71fe8b1c1f71ce26bf6cf6e613e8cd233ffa7e4e
dmoldova <dmoldova@usc.edu>
2019-11-16 11:29:59 -0800

    Started data study and stratification

---
+++ Manipulation.py
@@ -0,0 +1,10 @@
+#Processing the data
+
+import librosa
+import numpy as np
+import pandas as pd
+from scipy.io import wavfile as wav
+
+#What kind of data are we working with?
+data = pd.read_csv('UrbanSound8K/metadata/UrbanSound8K.csv')
+print(data.class_name.value_counts())
